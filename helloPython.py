35aed0cab62020659f5a052ea4b272cdd0a3e61d
Dcrudup1 <144864820+Dcrudup1@users.noreply.github.com>
2023-11-21 19:43:57 -0800

    Add files via upload

---
+++ helloPython.py
@@ -0,0 +1,29 @@
+#let name ="dedrick"
+name = "crudup"
+total = 12.50
+found = False
+age = 37
+print (name)
+
+#if statement
+
+
+#if(i=0; i<10; i++){}
+if age < 50:
+    print('well looks like you are below 50 year old')
+elif age > 50:
+    print("well looks like you are 50 years old")
+print("well looks like im outside of the if statment")
+
+#functions
+def say_hello():
+    print("hello")
+
+def say_goodbye(name):
+    print("goodbye "+name)
+
+    #call the function
+say_hello()
+say_goodbye("dedrick")
+
+#functions (){some logic here}
---
+++ test.py
@@ -0,0 +1,19 @@
+# dictionaries
+user = {
+    "name": "Dedrick",
+    "lastName": "Crudup",
+    "age": 24,
+}
+print(user)
+print(type(user))
+print(user["name"] + " " + user["lastName"])
+
+# array but in Python, this is a list
+numbers = [1, 2, 3]  # Rename variable to "numbers" for clarity
+print(numbers)
+numbers.append(4)
+print(numbers)
+
+print(len(numbers))#count items
+print(len(user["name"]))#count characters
+print(len(user))
